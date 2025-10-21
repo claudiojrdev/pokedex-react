c650964b1334e0e801b4fe1fd451bf513e5ed524
Claudio <claudioalvescostajunior@gmail.com>
2025-10-21 15:18:54 -0300

    configuracao pokeApi com axios

--- src/App.tsx
+++ src/App.tsx
@@ -3,41 +3,45 @@ import SearchBar from './components/SearchBar'
 import PokeGallery from './components/PokeGallery'
 import { type Pokemon } from './types/DataTypes'
 import { useEffect, useState } from 'react'
+import axios from "axios"
+
+interface PokemonListResponse {
+  results: {name:string, url: string}[]
+}
 
 function App() {
+  const POKEDEX_LIST_URL = "https://pokeapi.co/api/v2/pokemon?limit=100"
+  
+  const [loading,setLoading] = useState<boolean>(false)
   const [allPokemons,setAllPokemons] = useState<Pokemon[]>([])
   const [filteredPokemons,setFilteredPokemons] = useState<Pokemon[]>([])
 
   useEffect(()=>{
-    const dataFromApi = [
-              {
-              id: 1,
-              name: "Bulbassaur",
-              img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-              },
-             {
-              id: 2,
-              name: "Bulbassaur",
-              img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"
-              },
-             {
-              id: 3,
-              name: "Bulbassaur",
-              img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png"
-              },
-              {
-              id: 4,
-              name: "Charmander",
-              img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
-              },
-              {
-              id: 5,
-              name: "Charmeleon",
-              img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"
-              }
-            ]
-    setAllPokemons(dataFromApi)
-    setFilteredPokemons(dataFromApi)
+    const fetchPokemons = async () => {
+      try {
+        setLoading(true)
+        
+        const listResponse = await axios.get<PokemonListResponse>(POKEDEX_LIST_URL)
+        const pokemonsUrls = listResponse.data.results.map(pokemon => pokemon.url) 
+
+        const pokemonPromises = pokemonsUrls.map((url)=> axios.get(url))
+        const pokemonDetails = await Promise.all(pokemonPromises)
+        
+        const newPokemons: Pokemon[] = pokemonDetails.map((Response => {
+          const pokeApi = Response.data;
+          return {
+            id: pokeApi.id,
+            name: pokeApi.name,
+            img: pokeApi.sprites.front_default
+          }
+        }))
+        setAllPokemons(newPokemons)
+        setFilteredPokemons(newPokemons)
+        setLoading(false)
+      } catch(err){
+        console.error(err)
+      }}
+    fetchPokemons()
   },[])
 
   const handleSearch = (searchText: string) => {
